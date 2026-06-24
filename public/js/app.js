f9de252c474777b77584bb7e2be2fadcca9d6b07
avnassonov-rgb <av.nassonov@gmail.com>
2026-06-24 09:39:02 +0500

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -75,7 +75,7 @@
     if (stamp) {
       if (meta.lastSyncAt) {
         var dbg = meta.debug;
-        var dbgTxt = dbg ? (" · получено счетов: " + dbg.totalFetched + ", удалено: " + dbg.skippedDeleted + ", отгружено: " + dbg.skippedShipped + ", без номера: " + dbg.skippedNoNumber + ", реализаций: " + dbg.realizCount) : "";
+        var dbgTxt = dbg ? (" · получено счетов: " + dbg.totalFetched + ", удалено: " + dbg.skippedDeleted + ", отгружено: " + dbg.skippedShipped + ", без номера: " + dbg.skippedNoNumber + ", реализаций: " + dbg.realizCount + ", в справочнике товаров: " + dbg.nomCount + ", не распознано позиций: " + dbg.unknownItemsCount) : "";
         stamp.textContent = (meta.lastSyncOk === false ? "Ошибка: " + (meta.lastSyncError || "") + " · посл. попытка " : "Обновлено: ") + fmtDateTime(meta.lastSyncAt) + dbgTxt;
       } else {
         stamp.textContent = "";
